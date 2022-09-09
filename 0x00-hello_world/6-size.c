659273ae692c87802aa539eac0902c567ccabe60
Yotorab <yotorab12512@gmail.com>
2022-09-09 10:04:47 -0700

    6. Size is not grandeur

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *mian - Printing the size of many var type
+ *Return: Alwayes 0 (Successful)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a chiar: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+}
