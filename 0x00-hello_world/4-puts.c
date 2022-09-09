3a5b78fb72ec9d94a7b545ce3c4b5e6bf8170563
Yotorab <yotorab12512@gmail.com>
2022-09-09 09:14:43 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
- /**
+/**
+ * main - Entry point
  *
- *mian - Printing 
- *
- * 
- *Return: Always 0 (Successful)
- *
+ *Return: Always 0 (Success)
+ *     
  */
 int main(void)
 {
