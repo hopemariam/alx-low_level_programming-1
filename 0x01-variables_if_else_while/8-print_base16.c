aac3ab85171b96f419a43e018f642c020389a1be
Yotorab <yotorab12512@gmail.com>
2022-09-12 06:01:16 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,13 @@ int main(void)
         char c;                                                                                                         
                                                                                                                         
         for (c = '0'; c <= '9'; c++)                                                                                    
-        {                                                                                                               
-                putchar(c);                                                                                             
+        {                                                                                                           
+	putchar(c);                                                                                             
         }      
 		
         for (c = 'a'; c <= 'f'; c++)                                                                                    
-        {                                                                                                               
-                putchar(c);                                                                                             
+        {                                                                                                              
+	putchar(c);                                                                                             
         }                                                                                                               
           	
         putchar('\n');                                                                                                  
