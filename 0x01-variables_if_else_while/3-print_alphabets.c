08e292df18538571822ab0d49744acc7b8cec523
Yotorab <yotorab12512@gmail.com>
2022-09-12 03:57:02 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+*main - lowercase alphabets
+*
+*Return: Always (success)
+*/
+int main(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{		
+	putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+	putchar(c);
+	}
+	
+	putchar('\n');
+
+	return (0);
+}
