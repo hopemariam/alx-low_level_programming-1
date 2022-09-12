6753c943d1e23f9ab6e66f67f391c5aa6e03fe3f
Yotorab <yotorab12512@gmail.com>
2022-09-12 01:55:45 -0700

    1. The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+
+/**
+*main - Print if number is positive, zero or negative
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+int n;
+char lsat[] = "Last digit of";
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+	printf("%s %d is ", last,n,n % 10);
+
+	if (n % 10 > 5)
+	{
+		printf("Greator than 5\n");
+	}
+	else if (n % 10 == 0)
+	 
+	{
+		printf("0\n");
+	}
+	else
+	{
+		printd("less than 6 and not 0\n");
+	}
+	return (0);
+
+}
