a7daf20bba114e11051a07404fbab2ec10cef5de
Yotorab <yotorab12512@gmail.com>
2022-09-12 02:30:08 -0700

    1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,7 +10,7 @@
 int main(void)
 {
 	int n;
-	char last[] = "Last digit of";
+	char last[]= "Last digit of";
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
@@ -19,7 +19,7 @@ int main(void)
 
 	if (n % 10 > 5)
 	{
-		printf("Greator than 5\n");
+		printf("greator than 5\n");
 	}
 	else if (n % 10 == 0)
 	 
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printd("less than 6 and not 0\n");
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 
