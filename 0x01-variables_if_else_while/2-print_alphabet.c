d5df8dab8ba73147d4e6518cfcead21643d7aa42
Yotorab <yotorab12512@gmail.com>
2022-09-12 03:29:01 -0700

    2. I sometimes suffer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdlib.h>
+
+/**
+*main - lowercase alphabets
+*
+*Return: Always (success)
+*/
+int main(void)
+{
+
+	char c;
+	
+	for (c = 'a'; c <= '2'; c++)
+	putchar(c);
+
+	putchar('\n');
+	return (0);
+}
