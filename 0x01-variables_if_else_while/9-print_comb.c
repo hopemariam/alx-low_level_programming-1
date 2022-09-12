5e4e12da288faf3d12a774f6f101529261bb69e7
Yotorab <yotorab12512@gmail.com>
2022-09-12 10:29:51 -0700

    9. Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,15 @@ int main(void)
 
 	for (c = '0'; c <= '9'; c++)
 	{
+
 	putchar(c);
 	
 	if (c != '9')	
 	{
-		putchar( ',');
+		putchar(',');
 		putchar(' ');	
 	}
-	}
+       }
 
 	putchar('\n');
 	return (0);
