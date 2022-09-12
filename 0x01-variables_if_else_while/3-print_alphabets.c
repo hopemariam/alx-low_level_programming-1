d97a27e6518fba09f8a70d0f0ea6c9811d33735a
Yotorab <yotorab12512@gmail.com>
2022-09-12 04:05:45 -0700

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,15 +11,15 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+
 	{		
-		putchar(c);
+	putchar(c);
 	}
 	for (c = 'A'; c <= 'Z'; c++)
 	{
-		putchar(c);
+	putchar(c);
 	}
 	
 	putchar('\n');
-
 	return (0);
 }
