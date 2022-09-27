b7949d0ddf8d759e54cf6c8df54a86947ff9debf
Yotorab <yotorab12512@gmail.com>
2022-09-26 21:52:26 -0700

    0-main.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 /**
@@ -9,8 +8,8 @@
  */
 
 int _putchar(char c);
-void *_memset(void *s, int c, size_t n);
-void *_memcpy(void *dest, const void *src, size_t n);
+char *_memset(char *s, int c, size_t n);
+char *_memcpy(char *dest, const void *src, size_t n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
